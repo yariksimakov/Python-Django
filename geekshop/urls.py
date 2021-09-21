c4ffc7ab5254c107c4008df8002fb2b3e8912db7
yariksimakov <yariksimaov@mqil.ru>
2021-09-21 14:45:38 +0300

    Lesson_2_change_urls_for_products

--- geekshop/urls.py
+++ geekshop/urls.py
@@ -14,11 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from mainapp.views import index, products
+from django.urls import path, include
+from mainapp.views import index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index, name='index'),
-    path('products/', products, name='products'),
+    path('products/', include('mainapp.urls', namespace='products'))
 ]
