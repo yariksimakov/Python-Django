2ee6a142407a3bf99b130c4b7b2f1050bff69453
yariksimakov <yariksimaov@mqil.ru>
2021-09-23 22:30:34 +0300

    added media

--- geekshop/urls.py
+++ geekshop/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from mainapp.views import index
 from django.conf import settings
 from django.conf.urls.static import static
+
+
+from mainapp.views import index
 
 
 urlpatterns = [
