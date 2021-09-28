c6c6e8df230fd452f71f6cb1a80e62ec07654d8d
yariksimakov <yariksimaov@mqil.ru>
2021-09-28 09:04:10 +0300

    added startapp users

--- users/views.py
+++ users/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+def login(request):
+    context ={
+        "title": "Geekshop - Autorisation"
+    }
+    return render(request, 'users/login.html', context)
+
+def register(request):
+    context ={
+        "title": "Geekshop - Registration"
+    }
+    return render(request, 'users/register.html', context)
