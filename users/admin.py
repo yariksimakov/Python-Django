9fedc17ac0e5d3cdae6d78da5e31a8d41f74afcb
yariksimakov <yariksimaov@mqil.ru>
2021-10-06 20:58:05 +0300

    update admin

--- users/admin.py
+++ users/admin.py
@@ -1,5 +1,13 @@
 from django.contrib import admin
+
+from baskets.admin import BasketAdmin
+from baskets.models import Basket
 from .models import User
 # Register your models here.
 
 admin.site.register(User)
+
+
+class UserAdmin(admin.ModelAdmin):
+    model = Basket
+    inlines = (BasketAdmin, )
