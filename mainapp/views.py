824be14a71fc05d19a5850bf57a51dd1dc80cc79
yariksimakov <yariksimaov@mqil.ru>
2021-10-11 13:09:39 +0300

    bug for mainapp change on the CBV

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,5 +1,7 @@
 from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from django.shortcuts import render
+from django.views.generic import ListView
+
 from mainapp.models import Products, ProductsCategory
 
 
@@ -9,6 +11,33 @@ from mainapp.models import Products, ProductsCategory
 def index(request):
     return render(request, 'mainapp/index.html')
 
+
+class ProductsListView(ListView):
+    model = Products
+    template_name = 'mainapp/products.html'
+    paginate_by = 3
+
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = "geekshop"
+        context['categorys'] = ProductsCategory.objects.all()
+        list_example = Products.objects.filter(
+            category=self.request.GET.get('category_id')) if self.request.GET.get('category_id') != None else Products.objects.all()
+        paginator = Paginator(list_example, self.paginate_by)
+
+        page = self.request.GET.get('page_id')
+
+        try:
+            file_exams = paginator.page(page)
+
+        except PageNotAnInteger:
+            file_exams = paginator.page(1)
+
+        except EmptyPage:
+            file_exams = paginator.page(paginator.num_pages)
+
+        context['products'] = file_exams
+        return context
 
 def products(request, category_id=None, page_id=1):
     products = Products.objects.filter(
